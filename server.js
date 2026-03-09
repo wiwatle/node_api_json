610670f94949fe8939eeff29dc41f0f2a1e92c11
wiwatle <tonn_lephon1@hotmail.com>
2026-03-09 13:29:15 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
+const DATA_FILE = './data.json';
+
+app.use(express.json());
+
+// Helper function to read/write (dryer code)
+const readData = () => JSON.parse(fs.readFileSync(DATA_FILE, 'utf8'));
+const writeData = (data) => fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+
+// --- ROUTES ---
+
+// GET: Fetch all items
+app.get('/items', (req, res) => {
+    res.json(readData());
+});
+
+// POST: Create a new item
+app.post('/items', (req, res) => {
+    const data = readData();
+    const newItem = { id: Date.now(), ...req.body };
+    data.push(newItem);
+    writeData(data);
+    res.status(201).json(newItem);
+});
+
+// PUT: Update an item by ID
+app.put('/items/:id', (req, res) => {
+    let data = readData();
+    const index = data.findIndex(i => i.id === parseInt(req.params.id));
+    
+    if (index !== -1) {
+        data[index] = { ...data[index], ...req.body };
+        writeData(data);
+        res.json(data[index]);
+    } else {
+        res.status(404).send("Item not found");
+    }
+});
+
+// DELETE: Remove an item
+app.delete('/items/:id', (req, res) => {
+    let data = readData();
+    const filteredData = data.filter(i => i.id !== parseInt(req.params.id));
+    writeData(filteredData);
+    res.status(204).send();
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
